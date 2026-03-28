4a5c396d4908afb9641b510519408e0c232e36ed
ehsan00 <ehsanmoloudi@gmail.com>
2026-03-28 16:01:58 +0330

    update index file

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
-const button = document.getElementById("read-more-btn");
-  const message = document.getElementById("message");
+const images = document.querySelectorAll(".gallery img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.querySelector(".close");
 
-  button.addEventListener("click", function() {
-    message.textContent = "This is te new paragraph";
-    message.style.color = "blue";
-  });
+// باز کردن مودال
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img.src;
+    });
+});
+
+// بستن با دکمه ❌
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// بستن با کلیک روی بیرون عکس
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
